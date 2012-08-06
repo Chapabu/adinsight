33543dbc0e7b82e93dd6dfadab24e2f78471248c
Matt Chapman <kittymangler@gmail.com>
2012-08-07 00:16:05 +0100

    added javascript and sorted out Drupal.settings/behaviours

---
+++ adinsight.js
@@ -0,0 +1,18 @@
+(function($){
+
+  Drupal.behaviors.adInsight = {
+    attach: function(context, settings) {
+      adiInit = settings.adinsight['id'];
+
+      (function() {
+        var adiSrc = document.createElement("script");
+        adiSrc.type = "text/javascript";
+        adiSrc.async = true;
+        adiSrc.src = ("https:" == document.location.protocol ? "https://"
+          : "http://") + "static.adinsight.com/static/scripts/adiTrack.min.js";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(adiSrc, s);
+      })();
+    }
+  }
+})(jQuery);
